eb39faf1b17791d2559fb82c7998bddd6ea468f8
Carl Magnuson <Carl.Magnuson@mylaps.com>
2017-04-14 16:07:51 -0400

    Convert from String->Text internally, prepare for testing

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,12 @@
+--import           CsvDiffImpl
+--import           Test.QuickCheck
+
 main :: IO ()
 main = putStrLn "Test suite not yet implemented"
+--main = quickCheck prop_columnAdditionsDeletions
+
+-- prop_columnAdditionsDeletions :: IndexedCsv -> IndexedCsv -> Bool
+-- prop_columnAdditionsDeletions csv1 csv2 = findDeletedColumns csv1 csv2 == findAddedColumns csv2 csv1
+--
+-- prop_additionsDeletions :: IndexedCsv -> IndexedCsv -> Bool
+-- prop_additionsDeletions csv1 csv2 = findDeletes csv1 csv2 == findAdds csv2 csv1
